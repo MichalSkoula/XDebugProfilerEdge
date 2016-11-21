39968a15c36a8d64d7ef593f6df6400d4a0e5209
MichalSkoula <michal@exitshop.cz>
2016-11-21 16:56:19 +0100

    use cookies instead of get

--- js/functions.js
+++ js/functions.js
@@ -1,56 +1,12 @@
-////////////////////////////////////////////////////////////////////////////////
-
-// http://stackoverflow.com/questions/1634748/how-can-i-delete-a-query-string-parameter-in-javascript
-function removeURLParameter(url, parameter) {
-    var urlparts= url.split('?');   
-    if (urlparts.length>=2) {
-
-        var prefix= encodeURIComponent(parameter)+'=';
-        var pars= urlparts[1].split(/[&;]/g);
-
-        for (var i= pars.length; i-- > 0;) {    
-            if (pars[i].lastIndexOf(prefix, 0) !== -1) {  
-                pars.splice(i, 1);
-            }
-        }
-        url= urlparts[0] + (pars.length > 0 ? '?' + pars.join('&') : "");
-        return url;
-    } else {
-        return url;
-    }
-}
-//http://stackoverflow.com/questions/6953944/how-to-add-parameters-to-a-url-that-already-contains-other-parameters-and-maybe
-function addParam(url, param, value) {
-   var a = document.createElement('a'), regex = /(?:\?|&amp;|&)+([^=]+)(?:=([^&]*))*/g;
-   var match, str = []; a.href = url; param = encodeURIComponent(param);
-   while (match = regex.exec(a.search))
-       if (param != match[1]) str.push(match[1]+(match[2]?"="+match[2]:""));
-   str.push(param+(value?"="+ encodeURIComponent(value):""));
-   a.search = str.join("&");
-   return a.href;
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-
-function changeUrl(){
+function changeCookie(){
     browser.tabs.query({ currentWindow: true, active: true }, function(tabs){
-        var url = tabs[0].url;
-        if(url.indexOf("XDEBUG_PROFILE") > -1) {
-            var newUrl = removeURLParameter(url,"XDEBUG_PROFILE");
-        }
-        else {
-            var newUrl = addParam(url,"XDEBUG_PROFILE","1");
-        } 
-
         updateProperties = new Object();
-        updateProperties.url = newUrl;
-        browser.tabs.update(tabs[0].id, updateProperties, function() {
-            // Anything else you want to do after the tab has been updated.
-        });     
+        updateProperties.code = 'if(document.cookie.indexOf("XDEBUG_PROFILE") > -1) document.cookie = "XDEBUG_PROFILE=; expires=Thu, 01 Jan 1970 00:00:00 UTC"; else document.cookie = "XDEBUG_PROFILE=1";';
+        browser.tabs.executeScript(tabs[0].id, updateProperties, function() {
+            
+        });  
     });
     window.close();
 };
 
-// Event binding.
-browser.browserAction.onClicked.addListener(changeUrl());
+browser.browserAction.onClicked.addListener(changeCookie());
